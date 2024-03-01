dd15901626f8a4fa9f8822d74cf241833f5052ec
Lian <lliao@email.sc.edu>
2024-03-01 17:51:48 -0500

    0301

---
+++ detectonly.py
@@ -0,0 +1,129 @@
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import time
+from threading import Thread
+import importlib.util
+
+class VideoStream:
+    def __init__(self, resolution=(640,480), framerate=30):
+        self.stream = cv2.VideoCapture(0)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3, resolution[0])
+        ret = self.stream.set(4, resolution[1])
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while True:
+            if self.stopped:
+                self.stream.release()
+                return
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir', required=True)
+parser.add_argument('--graph', default='detect.tflite')
+parser.add_argument('--labels', default='labelmap.txt')
+parser.add_argument('--threshold', default=0.2)
+parser.add_argument('--resolution', default='640x480')
+parser.add_argument('--edgetpu', action='store_true')
+
+args = parser.parse_args()
+
+MODEL_NAME = args.modeldir
+GRAPH_NAME = args.graph
+LABELMAP_NAME = args.labels
+min_conf_threshold = float(args.threshold)
+resW, resH = args.resolution.split('x')
+imW, imH = int(resW), int(resH)
+use_TPU = args.edgetpu
+
+pkg = importlib.util.find_spec('tflite_runtime')
+if pkg:
+    from tflite_runtime.interpreter import Interpreter
+    if use_TPU:
+        from tflite_runtime.interpreter import load_delegate
+else:
+    from tensorflow.lite.python.interpreter import Interpreter
+    if use_TPU:
+        from tensorflow.lite.python.interpreter import load_delegate
+
+if use_TPU:
+    if GRAPH_NAME == 'detect.tflite':
+        GRAPH_NAME = 'edgetpu.tflite'
+
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
+PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
+
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+if labels[0] == '???':
+    del(labels[0])
+
+if use_TPU:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT,
+                              experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+else:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
+
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+input_mean = 127.5
+input_std = 127.5
+
+videostream = VideoStream(resolution=(imW,imH), framerate=30).start()
+time.sleep(1)
+
+while True:
+    frame1 = videostream.read()
+    frame_rgb = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    boxes = interpreter.get_tensor(output_details[1]['index'])[0]
+    classes = interpreter.get_tensor(output_details[3]['index'])[0]
+    scores = interpreter.get_tensor(output_details[0]['index'])[0]
+
+    detected = False  # Flag to check if any object is detected
+    for i in range(len(scores)):
+        if scores[i] > min_conf_threshold:
+            object_name = labels[int(classes[i])]
+            print(f"Detected {object_name} with confidence {scores[i]*100:.2f}%")
+            detected = True
+
+    if not detected:
+        print("Nothing detected")
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+    time.sleep(1)  # Wait for 1 second before the next detection
+
+videostream.stop()
